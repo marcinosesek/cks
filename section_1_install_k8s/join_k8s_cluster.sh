5e63dd21fc299e9d533cd8bc4f718450d164dff7
Marcin Osesek <marcin_osesek@o2.pl>
2021-12-10 22:30:39 +0100

    Small update

--- section_1_install_k8s/join_k8s_cluster.sh
+++ section_1_install_k8s/join_k8s_cluster.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.2:6443 --token v92mk6.zcfxkc5mqqu30fjd --discovery-token-ca-cert-hash sha256:2e7d17d31507579930677e8648099f274f131203762a32d8b91d4168f5d90081 
+kubeadm join 192.168.56.2:6443 --token 4oqdgx.6m2relra890peap8 --discovery-token-ca-cert-hash sha256:1ea7ed10df71ec71f093c9131c7dd201f5ce2f9f12500eacacdd9a27647dfc76 
